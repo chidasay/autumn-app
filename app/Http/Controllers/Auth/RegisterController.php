509149b6c2e1911ff008ecc010ad029aebcae480
chidasay <chidasay@gmail.com>
2026-01-22 14:14:02 +0900

    ログインフォームのバリデーションとデータ挿入

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,27 +3,37 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
     public function create(Request $request)
     {
-        // Create user registration logic here
         return view('auth.register');
     }
 
     public function store(Request $request)
     {
-        // Store user registration logic here
-        // バリデーション
-        // 次はここから！ https://kinsta.com/jp/blog/laravel-authentication/
+        /*
+        Validation 
+        */
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:8',
+        ]);
 
-        // $request->validate([
-        //     'name' => 'required|string|max:255',
-        //     'email' => 'required|email|unique:users,email',
-        //     'password' => 'required|confirmed|min:8',
-        // ]);
+        /*
+        Database Insertion
+        */
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
 
+        return back();
     }
 }
